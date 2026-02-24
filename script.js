c02c524fd56d547443ff39de35ed843fd64aa0e1
mjcw88 <144539369+mjcw88@users.noreply.github.com>
2026-02-24 21:30:04 +0000

    refined playTurn function, added checkWinner and isTie functions

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // Game board IIFE
     const gameBoard = (() => {
         const board = [
-            ["", "", ""],
-            ["", "", ""],
-            ["", "", ""],
+            [ "", "", ""],
+            [ "", "", ""],
+            [ "", "", ""],
         ];
         return { board };
     })();
@@ -15,88 +15,123 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function createGame(board) {
+        const BOARD_SIZE = gameBoard.board.length;
+
+        // Potententially move these into a start game function
         const playerOne = createPlayer("Player 1", 1, "X", true);
         const playerTwo = createPlayer("Player 2", 2, "O", true);
 
-        const playRound = () => {
-            console.log(board);
-            const square = parseInt(prompt("Choose a square"));
+        let playerOneTurn = true;
 
-            switch(square) {
-                case 1:
-                    // if player one turn
-                    gameBoard.board[0][0] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[0][0] = playerTwo.icon;
-                    break
-                case 2:
-                    // if player one turn
-                    gameBoard.board[0][1] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[0][1] = playerTwo.icon;
-                    break
-                case 3:
-                    // if player one turn
-                    gameBoard.board[0][2] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[0][2] = playerTwo.icon;
-                    break
-                case 4:
-                    // if player one turn
-                    gameBoard.board[1][0] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[1][0] = playerTwo.icon;
-                    break
-                case 5:
-                    // if player one turn
-                    gameBoard.board[1][1] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[1][1] = playerTwo.icon;
-                    break
-                case 6:
-                    // if player one turn
-                    gameBoard.board[1][2] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[1][2] = playerTwo.icon;
-                    break
-                case 7:
-                    // if player one turn
-                    gameBoard.board[2][0] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[2][0] = playerTwo.icon;
-                    break
-                case 8:
-                    // if player one turn
-                    gameBoard.board[2][1] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[2][1] = playerTwo.icon;
-                    break
-                case 9:
-                    // if player one turn
-                    gameBoard.board[2][2] = playerOne.icon;
-                    // if player two turn
-                    // gameBoard.board[2][2] = playerTwo.icon;
-                    break
+        const promptUser = () => {
+            let player;
+            playerOneTurn ? player = playerOne.name : player = playerTwo.name;
+            return parseInt(prompt(`${player}, choose a square:`)) - 1;
+        }
+
+        const playTurn = () => {
+            while (true) {
+                let chosenSquare = promptUser();
+
+                let row = Math.floor(chosenSquare / BOARD_SIZE);
+                let col = chosenSquare % BOARD_SIZE;
+
+                if (gameBoard.board[row][col] === "") {
+                    playerOneTurn ? gameBoard.board[row][col] = playerOne.icon : gameBoard.board[row][col] = playerTwo.icon;
+                    playerOneTurn = !playerOneTurn;
+                    break;
+                }
             }
         }
 
-        return { board, playerOne, playerTwo, playRound };
+        const checkWinner = () => {
+            const players = [playerOne.icon, playerTwo.icon];
+
+            // Checks rows
+            for (let i = 0; i < players.length; i++) {
+                for (let j = 0; j < BOARD_SIZE; j++) {
+                    if (gameBoard.board[j].every(row => row === players[i])) {
+                        return players[i];
+                    }
+                }
+            }
+
+            // Checks columns
+            for (let i = 0; i < players.length; i++) {
+                for (let j = 0; j < BOARD_SIZE; j++) {
+                    if (gameBoard.board.every(row => row[j] === players[i])) {
+                        return players[i];
+                    }
+                }
+            }
+
+            // Checks top left - bottom right diagonal
+            const topLeftDiag = []
+            for (let i = 0; i < BOARD_SIZE; i++) {
+                topLeftDiag.push(gameBoard.board[i][i]);
+            }
+
+            for (let i = 0; i < players.length; i++) {
+                if (topLeftDiag.every(row => row === players[i])) {
+                    return players[i];
+                }
+            }
+
+            // Checks top right - bottom left diagonal
+            const topRightDiag = []
+            let j = BOARD_SIZE - 1;
+            for (let i = 0; i < BOARD_SIZE; i++) {
+                topRightDiag.push(gameBoard.board[i][j]);
+                j--;
+            }
+
+            for (let i = 0; i < players.length; i++) {
+                if (topRightDiag.every(row => row === players[i])) {
+                    return players[i];
+                }
+            }
+
+            return null;
+        }
+
+        const isTie = () => {
+            if (game.checkWinner() !== null) {
+                return false;
+            }
+
+            if (gameBoard.board.every(row => row.every(cell => cell !== ""))) {
+                return true;
+            }
+
+            return false;
+        }
+
+        // TODO
+        const resetGame = () => {
+
+        }
+
+        return { board, playerOne, playerTwo, playTurn, checkWinner, isTie, resetGame };
     }
 
     const game = createGame(gameBoard);
 
-    game.playRound();
+    for (let i = 0; i < 9; i++) {
+        game.playTurn();
 
-    // play game
-        // player 1 selects tile
-        // check if tile is available, if so assign it to player
-        // check every row / column / diagonals for 3 in a row (player 1 wins)
-        // check if there any blank tiles left (tie)
-        
-        // player 2 selects tile
-        // check if tile is available, if so assign it to player
-        // check every row / column / diagonals for 3 in a row (player 2 / cpu wins)
-        // check if there any blank tiles left (tie)
+        console.log(`${gameBoard.board[0][0]} | ${gameBoard.board[0][1]} | ${gameBoard.board[0][2]}`);
+        console.log(`${gameBoard.board[1][0]} | ${gameBoard.board[1][1]} | ${gameBoard.board[1][2]}`);
+        console.log(`${gameBoard.board[2][0]} | ${gameBoard.board[2][1]} | ${gameBoard.board[2][2]}`);
+        console.log(`-------------------------`);
 
-        // repeat until winner
+        if (game.checkWinner() !== null) {
+            console.log(`${game.checkWinner()} WINS`);
+            break;
+        }
+
+        if (game.isTie()) {
+            console.log(`IT'S A TIE!`);
+            break;
+        }
+    }
 });
